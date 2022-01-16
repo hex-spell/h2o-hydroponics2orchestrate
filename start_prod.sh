6e0f6758ac4848f3cc0704c81335f15b0f0c72cc
Modulariz <agustin.favo@gmail.com>
2022-01-16 15:16:14 -0300

    add detached arg to prod start script

--- start_prod.sh
+++ start_prod.sh
@@ -1,1 +1,6 @@
-sudo docker-compose up mysql redis h2o_hydroponics_published
+sudo docker pull ghcr.io/modulariz/h2o-hydroponics2orchestrate:main
+if [ $1 = "detached" ]; then
+   sudo docker-compose up -d mysql redis h2o_hydroponics_published
+else
+   sudo docker-compose up mysql redis h2o_hydroponics_published
+fi
