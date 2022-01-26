6549303b48d9b5aaf459b5cf445312a653dde718
Modulariz <agustin.favo@gmail.com>
2022-01-26 02:02:41 -0300

    fix cors error

--- src/main/kotlin/com/modulariz/h2ohydroponics2operate/sensorstate/SensorStateController.kt
+++ src/main/kotlin/com/modulariz/h2ohydroponics2operate/sensorstate/SensorStateController.kt
@@ -1,12 +1,14 @@
 package com.modulariz.h2ohydroponics2operate.sensorstate
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 @RestController
+@CrossOrigin(origins = ["*"])
 @RequestMapping("/sensors")
 class SensorStateController {
     @Autowired
